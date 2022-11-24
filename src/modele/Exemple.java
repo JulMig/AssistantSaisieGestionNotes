ff2cc92e6f22ee69d6938859988e6c0e9d435584
JulMig <julie.mignotte8@gmail.com>
2022-11-24 14:08:07 +0100

    Ajout Package/Classe
    Création de la structure principale du projet.

---
+++ src/modele/Exemple.java
@@ -0,0 +1,10 @@
+package modele;
+
+public class Exemple {
+	
+	//TODO scénario type pour montrer l'utilisation des fonctions en tant qu'utilisateur
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/modele/Test.java
@@ -0,0 +1,12 @@
+package modele;
+
+public class Test {
+	
+	//TODO différente méthode d'assertion pour les différentes classe
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
